48d2a0a46c21db282e16ef028867c82dbd1369e4
RobertLynch21 <r.f.lynch@gmail.com>
2025-04-01 21:19:18 -0400

    updated api.js for backend hosting on render

--- frontend/to-do/src/api/api.js
+++ frontend/to-do/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000", // Backend URL
+    baseURL: "https://to-do-backend.onrender.com", // Backend URL
 });
 
 export default API
