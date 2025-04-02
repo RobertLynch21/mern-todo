d3305109b53ae5808606458a7e42859440e49b91
RobertLynch21 <r.f.lynch@gmail.com>
2025-04-01 21:30:35 -0400

    pointed the api to the back end

--- frontend/to-do/src/api/api.js
+++ frontend/to-do/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://to-do-backend.onrender.com", // Backend URL
+    baseURL: "https://mern-todo.onrender.com", // Backend URL
 });
 
 export default API
