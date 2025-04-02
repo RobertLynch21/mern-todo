7717ec61510657eeb11e33e9cea0a814fbec65b1
RobertLynch21 <r.f.lynch@gmail.com>
2025-04-01 21:32:53 -0400

    pointed the api to the back enddue to config error

--- frontend/to-do/src/api/api.js
+++ frontend/to-do/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://mern-todo.onrender.com", // Backend URL
+    baseURL: "https://mern-todo-backend-syas.onrender.com", // Backend URL
 });
 
 export default API
