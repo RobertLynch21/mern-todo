dae0b66f2ceec365c58e59f292bb591dec053d3f
RobertLynch21 <r.f.lynch@gmail.com>
2025-04-04 15:51:46 -0400

    Completed todo's logic and added the input form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,41 @@ import { useEffect, useState } from "react";
 import API from "./api/api"
 
 function App(){
-  const [message, setMessage] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
 
   useEffect(() => {
-    API.get("/")
-      .then((res) => setMessage(res.data))
-      .catch((err) => console.error("Error fetching data:", err));
+    API.get("/todos")
+      .then((res) => setTodos(res.data))
+      .catch((err) => console.error("Error fetching todos:", err));
   }, []);
+
+  const handleSubmit = () => {
+    API.post("/todos", { text })
+    .then((res) => setTodos([...todos, res.data]))
+    .catch((err) => console.err("Error adding todo:", err));
+    setText("");
+  };
 
   return(
     <div>
       <h1>Todo App</h1>
       <h2>This is an ongoing project. Check back daily for updates.</h2>
-      <p>Backend says: {message}</p>
+      
+      {/*Form to add a todo */}
+      <input 
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder="Add a Todo"
+      />
+      <button onClick={handleSubmit}>Add</button>
+      
+      {/*Display todos*/}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
